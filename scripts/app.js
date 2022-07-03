d3df75fa349be936738a64b53559938a738564c3
Neeraj Kumar Yadav <64354268+yaduvanshiNK@users.noreply.github.com>
2022-07-03 23:47:34 +0530

    hamburgerWorking

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,19 @@
+//Hamburegr fucntionality
+const hambuerger = document.querySelector(".hamburger-menu");
+const hambuergerMoboScreen = document.querySelector(
+  ".hamburger-item-container"
+);
+hambuerger.addEventListener("click", () => {
+  hambuergerMoboScreen.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  hambuergerMoboScreen.addEventListener("click", (e) => {
+    hambuergerMoboScreen.style.display = "none";
+    document.body.style.overflowY = "scroll";
+  });
+});
+
+//Card Animation
+
 //***********Scroll Trigger GSAP Animations********
 
 const headerTimeline = gsap.timeline({
@@ -13,4 +29,5 @@ headerTimeline
   .from(".left-nft", { x: 100, rotate: "30deg", opacity: 0 }, "-=.2")
   .from(".right-nft", { x: -100, rotate: "-30deg", opacity: 0 }, "-=.5")
   .from(".kizuki-heading", { y: "100%", opacity: 0 }, "-=.6")
-  .from(".homepage-left-container p ", { y: "100%", opacity: 0 }, "-=.8");
+  .from(".homepage-left-container p ", { y: "100%", opacity: 0 }, "-=.8")
+  .from(".header-btns a ", { y: "100%", opacity: 0 }, "-=.5");
